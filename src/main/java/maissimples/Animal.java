aa575c8449504a933a3ab668c4abc03831752282
adrianlucastdk <adrianlucastoderke@hotmail.com>
2023-03-08 20:14:20 -0300

    Primeiro

---
+++ src/main/java/maissimples/Animal.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maissimples;
+
+/**
+ *
+ * @author aluno
+ */
+public class Animal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pato Pato1 = new Pato();
+        Pato1.nome = ("Joao");
+        Pato1.cor = ("Branco");
+        Pato1.comportamento = ("Calmo");
+        Pato1.sexo = ("Masculino");
+        Pato1.classe = ("Aves");
+        
+        Pato Pato2 = new Pato();
+        Pato2.nome = ("Jonas");
+        Pato2.cor = ("Preto");
+        Pato2.comportamento = ("Passivo");
+        Pato2.sexo = ("Masculino");
+        Pato2.classe = ("Aves");
+        
+        Pato Pato3 = new Pato();
+        Pato3.nome = ("Psiu");
+        Pato3.cor = ("Cinza");
+        Pato3.comportamento = ("Bravo");
+        Pato3.sexo = ("Feminino");
+        Pato3.classe = ("Aves");
+        
+        Pato Pato4 = new Pato();
+        Pato4.nome = ("Faiado");
+        Pato4.cor = ("Preto");
+        Pato4.comportamento = ("Agressivo");
+        Pato4.sexo = ("Masculino");
+        Pato4.classe = ("Aves");
+        
+        Pato Pato5 = new Pato();
+        Pato5.nome = ("Mimizento");
+        Pato5.cor = ("Cinza e Preto");
+        Pato5.comportamento = ("Agressivo");
+        Pato5.sexo = ("Masculino");
+        Pato5.classe = ("Aves");
+    }
+    
+}
